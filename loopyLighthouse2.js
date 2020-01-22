d10353430c25217549c5a893f4c48e58df154df2
Richard <liao098@gmail.com>
2020-01-21 17:16:44 -0800

    loopyLighthouse2 fix

--- loopyLighthouse2.js
+++ loopyLighthouse2.js
@@ -1,5 +1,5 @@
 function loopyLighthouse(range, multiples, words) {
-  if (range.length === 2 && multiples.length === 2 && words.length === 2 &&) {
+  if (range.length === 2 && multiples.length === 2 && words.length === 2) {
     for (let i = range[0]; i <= range[1]; i++) {
       if (i % multiples[0] === 0 && i % multiples[1] === 0) {
         console.log(words[0] + words[1]);
